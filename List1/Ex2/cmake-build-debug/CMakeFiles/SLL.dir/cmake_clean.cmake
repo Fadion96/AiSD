7fae3572ad9dbba3e54f51166265bcac1a3c7217
Fadion96 <nowak196@gmail.com>
2017-03-24 13:03:41 +0100

    Added Ex2 from List2

---
+++ List1/Ex2/cmake-build-debug/CMakeFiles/SLL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SLL.dir/singlyLinkedList.c.obj"
+  "SLL.pdb"
+  "SLL.exe"
+  "SLL.exe.manifest"
+  "libSLL.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SLL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
